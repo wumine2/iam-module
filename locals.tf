88cc73e10947ea988a0ba7a726408694b472fa49
Wunmi <atiwunmi@yahoo.com>
2023-10-08 10:15:37 -0600

    locals blocked updated

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
   node_policies = [
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonECSContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
     "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
   ]
 }
